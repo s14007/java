43999126c19bfb6b029ee183abd227ec4279b7db
s14007 <s14007@std.it-college.ac.jp>
2014-10-14 12:12:03 +0900

    Ex2.java修正

--- Ex1.java
+++ Ex1.java
@@ -10,6 +10,12 @@ class Ex1_0 extends Ex {
 
 class Ex1_2 extends Ex {
 	void execute(){
+		System.out.println("高\n良\n裕\n太");
+	}
+}
+
+class Ex1_3 extends Ex {
+	void execute(){
 		System.out.println("高\n良\n\n裕\n太");
 	}
 }
@@ -20,6 +26,8 @@ public class Ex1 {
 		a = new Ex1_0();
 		a.execute();
 		a = new Ex1_2();
+                a.execute();
+                a = new Ex1_3();
 		a.execute();
 	}
 }
--- Ex2.java
+++ Ex2.java
@@ -7,6 +7,21 @@ abstract class Ex {
 
 class Ex2_0 extends Ex{
 	void execute(){
+		double x;
+		double y;
+
+		x = 63;
+		y = 18;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("合計は" + (x + y) + "です。");
+		System.out.println("平均は" + (x + y) / 2 + "です。");
+	}
+}
+
+class Ex2_1 extends Ex{
+	void execute(){
 		int x;
 		int y;
 
@@ -20,10 +35,32 @@ class Ex2_0 extends Ex{
 	}
 }
 
+class Ex2_2 extends Ex{
+	void execute(){
+		int x;
+		int y;
+                int z;
+
+		x = 63;
+		y = 18;
+                z = 20;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+                System.out.println("zの値は" + z + "です。");
+		System.out.println("合計は" + (x + y + z) + "です。");
+		System.out.println("平均は" + (x + y + z) / 2 + "です。");
+	}
+}
+
 public class Ex2 {
 	public static void main(String[] args){
 		Ex a;
 		a = new Ex2_0();
+		a.execute();
+                a = new Ex2_1();
+		a.execute();
+                a = new Ex2_2();
 		a.execute();
 	}
 }

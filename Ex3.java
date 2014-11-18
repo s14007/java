8b87fbb1937a7f7c46a321ecfb1920654a437668
s14007 <s14007@std.it-college.ac.jp>
2014-11-18 09:45:04 +0900

    java

--- Ex3.java
+++ Ex3.java
@@ -2,40 +2,83 @@ class Ex3{
 
 	public String ex3_0(int n){
 
-		return "dummy";
+          if(0 < n){
+            return "その値は正です"; 
+          }else{
+             return "その値は0か負です";
+          }
+
 
 	}
 
 	public int ex3_1(int n){
 		
-		return -1;
+          if(0 < n){
+             return n ;
+          }else{
+             return (n * -1); 
+          }
+        }
 
-	}
 
 	public String ex3_2(int a, int b){
 
-		return "dummy";
+          if(a / b == a){
+              return b + "は" + a + "の約数です";
+          }else{
+              return b + "は" + a + "の約数ではありません";
+          }
+
 	}
 
 	public String ex3_3(int a, int b){
 
-		return "dummy";
+          if(b < a){
+            return a + "のほうが大きいです";
+          }else if(a < b){
+            return b + "のほうが大きいです"; 
+          }else{
+            return "同じ値です";
+          }
+
 
 	}
 
 	public String ex3_4(int a){
 
-		return "dummy";
-
+          if(a / 5 == 0){
+             return "その値は5で割り切れます";
+          }else if(a == -a){
+            return "正でない整数値です";
+          }else{
+            return "その値は5で割り切れません";
+          }
 	}
 
 	public String ex3_5(int a){
 
-		return "dummy";
+          if(a * 10 < a){
+            return "その値は10の倍数です";
+          }else if(a == -a){
+            return "その値は10の倍数ではありません";
+          }else{
+             return "正でない整数値です";
+          }
 	}
 	
 	public String ex3_6(int a){
-		return "dummy";
+
+          if(a / 3 == 0){
+             return "その値は3で割り切れます";
+          }else if(a % 3 == 1){
+            return "その値を3で割った余りは1です";
+          }else if(a % 3 == 2){
+            return "その値を3割った余りは2です";
+          }else if(a == -a){
+            return "正でない整数値です";
+          }else{
+             return "error";
+          }
 	}
 
 	public String ex3_7(int a){

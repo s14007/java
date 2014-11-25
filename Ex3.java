f260faf062fb0bacea7efede0893531c045be755
s14007 <s14007@std.it-college.ac.jp>
2014-11-25 18:39:16 +0900

    java

--- Ex3.java
+++ Ex3.java
@@ -5,7 +5,7 @@ class Ex3{
     if(0 < n){
       return "その値は正です"; 
     }else{
-      return "その値は0か負です";
+      return "その値は０か負です";
     }
 
 
@@ -22,21 +22,22 @@ class Ex3{
 
 
   public String ex3_2(int a, int b){
-
-    if(a / b == a){
-      return b + "は" + a + "の約数です";
+     
+    if (a % b == 0){
+       return b + "は" + a + "の約数です";
     }else{
-      return b + "は" + a + "の約数ではありません";
+       return b + "は" + a + "の約数ではありません";
     }
-
   }
+
+
 
   public String ex3_3(int a, int b){
 
     if(b < a){
-      return a + "のほうが大きいです";
+      return a + "の方が大きいです";
     }else if(a < b){
-      return b + "のほうが大きいです"; 
+      return b + "の方が大きいです"; 
     }else{
       return "同じ値です";
     }
@@ -46,29 +47,31 @@ class Ex3{
 
   public String ex3_4(int a){
 
-    if(a / 5 == 0){
-      return "その値は5で割り切れます";
-    }else if(a == -a){
+    if(0 > a){
       return "正でない整数値です";
+    }else if(a % 5 == 0){
+      return "その値は5で割り切れます";
     }else{
       return "その値は5で割り切れません";
     }
+
   }
 
   public String ex3_5(int a){
 
-    if(a * 10 < a){
+
+    if(a % 10 == 0){
       return "その値は10の倍数です";
-    }else if(a == -a){
-      return "その値は10の倍数ではありません";
-    }else{
+    }else if(0 > a){
       return "正でない整数値です";
+    }else{
+      return "その値は10の倍数ではありません";
     }
   }
 
   public String ex3_6(int a){
 
-    if(a / 3 == 0){
+    if(a % 3 == 0){
       return "その値は3で割り切れます";
     }else if(a % 3 == 1){
       return "その値を3で割った余りは1です";
@@ -80,16 +83,16 @@ class Ex3{
   }
 
   public String ex3_7(int a){
-    if(0 < 59){
+    if(0 <= a && a <= 59){
       return "不可";
-    }else if(60 <69){
+    }else if(60 <= a && a <= 69){
       return "可";
-    }else if(70 < 79){
+    }else if(70 <= a && a <= 79){
       return "良";
-    }else if(80 < 100){
+    }else if(80 <= a && a <= 100){
       return "優";
     }else{
-      return "範囲(0~100)外の値です";
+      return "範囲(0～100)外の値です";
     }
   }
 
@@ -102,20 +105,18 @@ class Ex3{
   }
 
   public int ex3_9(int a, int b){
-    int num = 0;
-    if(a < num || num < b){
-      return num;
-    }else{
-      return 1;
-    }
+    int c = (a - b);
+    return c;
   }
 
   public String ex3_10(int a, int b){
 
-    if((a - b) < 10){
-      return "それらの差は10以下です";
-    }else{
+    int c = (a - b);
+
+    if(c > 10){
       return "それらの差は11以上です";
+    }else{
+      return "それらの差は10以下です";
     }
 
   }
